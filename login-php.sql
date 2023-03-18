7a4bc3ddbe287ba974d1eec982657b036d725f39
AngelDiaz591 <116088852+AngelDiaz591@users.noreply.github.com>
2023-03-18 12:46:13 -0600

    Add files via upload

--- login-php.sql
+++ login-php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-02-2023 a las 19:22:56
+-- Tiempo de generación: 18-03-2023 a las 19:41:25
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -24,45 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `usertable`
 --
 
-CREATE TABLE `usuarios` (
-  `ID` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `usuario` varchar(30) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `´privilegio` int(2) NOT NULL,
-  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+CREATE TABLE `usertable` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `code` mediumint(50) NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `usertable`
 --
 
-INSERT INTO `usuarios` (`ID`, `nombre`, `usuario`, `email`, `password`, `´privilegio`, `fecha_registro`) VALUES
-(1, 'Ismael Montiel', 'imontiel', 'imontiel@ucol.mx', '1234', 1, '2023-02-15 18:19:54');
+INSERT INTO `usertable` (`id`, `name`, `email`, `password`, `code`, `status`) VALUES
+(1, 'ISMAEL ALEJANDRO MONTIEL TORRES', 'imontiel@ucol.mx', '$2y$10$PmaD7KDJyqv5fgepoRL7uO1oJspzlW7Lfgz8GDZgFt/3WDXku3E.G', 0, 'verified'),
+(2, 'ANGEL GABRIEL DIAZ RAMIREZ', 'adiaz57@ucol.mx', '$2y$10$haQ5ru2MGpGQRm6s.kuqh.8WR0eqQHQMr8LtnaOgKXaaL7WjtGHdS', 0, 'verified'),
+(3, 'ALAN SAN MILLAN RAMOS', 'asanmillan@ucol.mx', '$2y$10$VtGxTlJ0UmMzkP0tB0GgGOG0HJDEDLc1XVmcmHtl0lkkdkkQdssEm', 0, 'verified'),
+(4, 'MARIANA IXCHEL PEGUEROS PEREZ', 'mpegueros@ucol.mx', '$2y$10$OeE5DmZcuj87WPD3XRwgluQ5fvw3CM/k6grPKr1PsIdwK.RXIHnJG', 0, 'verified'),
+(5, 'MAXIMILIANO ALEXANDER MENDOZA LOPEZ', 'mmendoza34@ucol.mx', '$2y$10$xSVNJI0aGfFlOBP2tdvT5.OVQuo2FOUkWXX41WTLOBLuf1oRAmdT.', 0, 'verified'),
+(6, 'JORGE GAEL VALENCIA COLIMA', 'jvalencia33@ucol.mx', '$2y$10$mSLk.VXG5fTBUnBMfOX3OOUDOEK9KlOI5y3aQ5IkowC7bMjzV0Wca', 0, 'verified');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `usertable`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `usertable`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `usertable`
 --
-ALTER TABLE `usuarios`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `usertable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
